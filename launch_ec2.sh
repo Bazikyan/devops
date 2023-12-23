d52024ac812d0ad1478a24463d15ff2a73f639e9
Bazikyan <bazikyan96@gmail.com>
2023-12-23 10:47:41 +0400

    fix

--- launch_ec2.sh
+++ launch_ec2.sh
@@ -6,13 +6,13 @@ region="eu-north-1"
 
 vpc_id=$(aws ec2 create-vpc --cidr-block='10.0.0.0/16' --region="$region" --query='Vpc.VpcId' --output=text)
 
-igw_id=$(aws ec2 create-internet-gateway --region="$region" --query='InternetGateway.InternetGatewayId')
+igw_id=$(aws ec2 create-internet-gateway --region="$region" --query='InternetGateway.InternetGatewayId' --output=text)
 
 aws ec2 attach-internet-gateway --vpc-id="$vpc_id" --internet-gateway-id="$igw_id" --region="$region"
 
-subnet_id=$(aws ec2 create-subnet --vpc-id="$vpc_id" --cidr-block='10.0.0.0/24' --region="$region" --query='Subnet.SubnetId')
+subnet_id=$(aws ec2 create-subnet --vpc-id="$vpc_id" --cidr-block='10.0.0.0/24' --region="$region" --query='Subnet.SubnetId' --output=text)
 
-route_table_id=$(aws ec2 create-route-table --vpc-id="$vpc_id" --region="$region" --query='RouteTable.RouteTableId')
+route_table_id=$(aws ec2 create-route-table --vpc-id="$vpc_id" --region="$region" --query='RouteTable.RouteTableId' --output=text)
 
 aws ec2 create-route --destination-cidr-block='0.0.0.0/0' --route-table-id="$route_table_id" --gateway-id="$igw_id" --region="$region"
 
