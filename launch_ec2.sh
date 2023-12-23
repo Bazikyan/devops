74d13315478ae5a0067ea20c59d54ae27d57ca4d
Bazikyan <bazikyan96@gmail.com>
2023-12-23 10:39:43 +0400

    fix

--- launch_ec2.sh
+++ launch_ec2.sh
@@ -4,46 +4,19 @@
 
 region="eu-north-1"
 
-vpc_id=$(aws ec2 create-vpc --cidr-block=10.0.0.0/16 --region=$region --query=Vpc.VpcId)
-echo "$vpc_id"
-igw_id=aws ec2 create-internet-gateway \
-  --region=$region \
-  --query=InternetGateway.InternetGatewayId
+vpc_id=$(aws ec2 create-vpc --cidr-block='10.0.0.0/16' --region="$region" --query='Vpc.VpcId')
 
-aws ec2 attach-internet-gateway \
-  --vpc-id=$vpc_id \
-  --internet-gateway-id=$igw_id \
-  --region=$region
+igw_id=$(aws ec2 create-internet-gateway --region="$region" --query='InternetGateway.InternetGatewayId')
 
-subnet_id=aws ec2 create-subnet \
-  --vpc-id=$vpc_id \
-  --cidr-block 10.0.0.0/24 \
-  --region=$region \
-  --query=Subnet.SubnetId
+aws ec2 attach-internet-gateway --vpc-id="$vpc_id" --internet-gateway-id="$igw_id" --region="$region"
 
-route_table_id=aws ec2 create-route-table \
-  --vpc-id=$vpc_id \
-  --region=$region \
-  --query=RouteTable.RouteTableId
+subnet_id=$(aws ec2 create-subnet --vpc-id="$vpc_id" --cidr-block='10.0.0.0/24' --region="$region" --query='Subnet.SubnetId')
 
-aws ec2 create-route \
-  --destination-cidr-block 0.0.0.0/0 \
-  --route-table-id=$route_table_id \
-  --gateway-id=$igw_id \
-  --region=$region
+route_table_id=$(aws ec2 create-route-table --vpc-id="$vpc_id" --region="$region" --query='RouteTable.RouteTableId')
 
-aws ec2 associate-route-table \
-  --subnet-id=$subnet_id \
-  --route-table-id=$route_table_id
-  --region=$region
+aws ec2 create-route --destination-cidr-block='0.0.0.0/0' --route-table-id="$route_table_id" --gateway-id="$igw_id" --region="$region"
 
-aws ec2 run-instances \
-  --image-id=ami-0fe8bec493a81c7da \
-  --count=1 \
-  --instance-type=t3.micro \
-  --key-name=devops \
-  --security-group-ids=sg-00b3e7fbc304ce07f \
-  --subnet-id=subnet_id \
-  --region=$region \
-  --vpc_id=$vpc_id
+aws ec2 associate-route-table --subnet-id="$subnet_id" --route-table-id="$route_table_id" --region="$region"
+
+aws ec2 run-instances --image-id='ami-0fe8bec493a81c7da' --count='1' --instance-type='t3.micro' --key-name='devops' --security-group-ids='sg-00b3e7fbc304ce07f' --subnet-id="$subnet_id" --region="$region" --vpc_id="$vpc_id"
 
