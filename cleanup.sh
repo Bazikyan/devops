cfc5965818a7ab699e4a123aa2767e2df34a2e18
Bazikyan <bazikyan96@gmail.com>
2023-12-27 10:38:37 +0400

    clean sg

--- cleanup.sh
+++ cleanup.sh
@@ -5,3 +5,9 @@
 region="eu-north-1"
 
 aws ec2 terminate-instances --instance-ids --region="$region" $(aws ec2 describe-instances --query "Reservations[?!(Instances[0].Tags && Instances[0].Tags[?Key=='usage' && Value=='permanent'])].Instances[*].InstanceId" --output text --region="$region")
+
+sg_ids=$(aws ec2 describe-security-groups --query "SecurityGroups[?!(Tags && Tags[?Key=='usage' && Value=='permanent'])].GroupId" --region="$region" --output text)
+
+for sg_id in $sg_ids; do
+    aws ec2 delete-security-group --region="$region" --group-id="$sg_id"
+done
