aecef840671d5a0878be9c73ad37977930283f7b
Bazikyan <bazikyan96@gmail.com>
2023-12-27 18:25:38 +0400

    fix

--- cleanup.sh
+++ cleanup.sh
@@ -5,7 +5,11 @@
 region="eu-north-1"
 
 # delete instances
-aws ec2 terminate-instances --instance-ids --region="$region" $(aws ec2 describe-instances --query "Reservations[?!(Instances[0].Tags && Instances[0].Tags[?Key=='usage' && Value=='permanent'])].Instances[*].InstanceId" --output text --region="$region")
+ec2_ids=$(aws ec2 describe-instances --query "Reservations[?!(Instances[0].Tags && Instances[0].Tags[?Key=='usage' && Value=='permanent'])].Instances[*].InstanceId" --output text --region="$region")
+
+if [ -n "$instance_ids" ]; then
+    aws ec2 terminate-instances --instance-ids $ec2_ids --region="$region"
+fi
 
 # delete security groups
 sg_ids=$(aws ec2 describe-security-groups --query "SecurityGroups[?!(Tags && Tags[?Key=='usage' && Value=='permanent']) && GroupName!='default'].GroupId" --region="$region" --output text)
