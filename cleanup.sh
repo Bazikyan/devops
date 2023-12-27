4be8b65a35ffb6d73c40a3eac700bb50284da06c
Bazikyan <bazikyan96@gmail.com>
2023-12-27 18:13:18 +0400

    clean route tables

--- cleanup.sh
+++ cleanup.sh
@@ -4,10 +4,19 @@
 
 region="eu-north-1"
 
+# delete instances
 aws ec2 terminate-instances --instance-ids --region="$region" $(aws ec2 describe-instances --query "Reservations[?!(Instances[0].Tags && Instances[0].Tags[?Key=='usage' && Value=='permanent'])].Instances[*].InstanceId" --output text --region="$region")
 
+# delete security groups
 sg_ids=$(aws ec2 describe-security-groups --query "SecurityGroups[?!(Tags && Tags[?Key=='usage' && Value=='permanent']) && GroupName!='default'].GroupId" --region="$region" --output text)
 
 for sg_id in $sg_ids; do
     aws ec2 delete-security-group --region="$region" --group-id="$sg_id"
+done
+
+# delete route tables
+route_table_ids=$(aws ec2 describe-route-tables --query "RouteTables[?!(Tags && Tags[?Key=='usage' && Value=='permanent'])].RouteTableId" --region="$region" --output text)
+
+for route_table_id in $route_table_ids; do
+    aws ec2 delete-route-table  --region="$region" --route-table-id="$route_table_id"
 done
