0317bc3d1c6711a0afd7f6e2f2bc37258c713542
Bazikyan <bazikyan96@gmail.com>
2023-12-27 09:32:27 +0400

    add sec group

--- launch_ec2.sh
+++ launch_ec2.sh
@@ -18,5 +18,9 @@ aws ec2 create-route --destination-cidr-block='0.0.0.0/0' --route-table-id="$rou
 
 aws ec2 associate-route-table --subnet-id="$subnet_id" --route-table-id="$route_table_id" --region="$region"
 
-aws ec2 run-instances --image-id='ami-0fe8bec493a81c7da' --count='1' --instance-type='t3.micro' --key-name='devops' --subnet-id="$subnet_id" --region="$region"
+security_group_id=$(aws ec2 create-security-group --group-name="MySecurityGroup" --description="My security group description" --vpc-id="$vpc_id" --output=text --query='GroupId')
+
+aws ec2 authorize-security-group-ingress --group-id="$security_group_id" --protocol='tcp' --port='22' --cidr='0.0.0.0/0'
+
+aws ec2 run-instances --image-id='ami-0fe8bec493a81c7da' --count='1' --instance-type='t3.micro' --key-name='devops' --subnet-id="$subnet_id" --security-group-ids="$security_group_id" --region="$region"
 
