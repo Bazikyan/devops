45191fd24e51490a113c77c72c655573e618b20c
Bazikyan <bazikyan96@gmail.com>
2024-01-20 14:44:27 +0400

    fix

--- cleanup.sh
+++ cleanup.sh
@@ -18,9 +18,23 @@ for sg_id in $sg_ids; do
     aws ec2 delete-security-group --region="$region" --group-id="$sg_id"
 done
 
-# delete route tables
-route_table_ids=$(aws ec2 describe-route-tables --query "RouteTables[?!(Tags && Tags[?Key=='usage' && Value=='permanent'])].RouteTableId" --region="$region" --output text)
+## delete route table
+#aws ec2 disassociate-route-table --association-id $(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpc_id" --region="$region" --query "RouteTables[?Associations[0].SubnetId!=''].Associations[0].RouteTableAssociationId" --output text) --region="$region"
+#aws ec2 delete-route-table --route-table-id="$route_table_id" --region="$region"
+#
+## delete subnet
+#aws ec2 delete-subnet --subnet-id="$subnet_id" --region="$region"
+#
+## detach and delete internet gateway
+#aws ec2 detach-internet-gateway --vpc-id="$vpc_id" --internet-gateway-id="$igw_id" --region="$region"
+#aws ec2 delete-internet-gateway --internet-gateway-id="$igw_id" --region="$region"
+#
+## delete VPC
+#aws ec2 delete-vpc --vpc-id="$vpc_id" --region="$region"
 
-for route_table_id in $route_table_ids; do
-    aws ec2 delete-route-table  --region="$region" --route-table-id="$route_table_id"
-done
+## delete route tables
+#route_table_ids=$(aws ec2 describe-route-tables --query "RouteTables[?!(Tags && Tags[?Key=='usage' && Value=='permanent'])].RouteTableId" --region="$region" --output text)
+#
+#for route_table_id in $route_table_ids; do
+#    aws ec2 delete-route-table  --region="$region" --route-table-id="$route_table_id"
+#done
